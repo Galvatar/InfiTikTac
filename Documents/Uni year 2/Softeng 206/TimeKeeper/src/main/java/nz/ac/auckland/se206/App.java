83eb7e2c0ece7821cd4746f580a968ed36e976b9
Michael Boyer <mboy221@aucklanduni.ac.nz>
2024-08-09 22:28:26 +1200

    First commit, with working gui but not features

---
+++ Documents/Uni year 2/Softeng 206/TimeKeeper/src/main/java/nz/ac/auckland/se206/App.java
@@ -0,0 +1,32 @@
+package nz.ac.auckland.se206;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+  private static Scene scene;
+
+  public static Parent loadFXML(String fxml) throws IOException {
+    FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/fxml/" + fxml + ".fxml"));
+    return fxmlLoader.load();
+  }
+
+  @Override
+    public void start(Stage stage) throws Exception {
+      Parent root = loadFXML("main");
+      
+      scene = new Scene(root);
+      stage.setScene(scene);
+      stage.show();
+      stage.setTitle("Window");
+      root.requestFocus();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ Documents/Uni year 2/Softeng 206/TimeKeeper/src/main/java/nz/ac/auckland/se206/controllers/mainController.java
@@ -0,0 +1,33 @@
+package nz.ac.auckland.se206.controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+public class mainController {
+    @FXML TextField name1;
+    @FXML TextField name2;
+    @FXML TextField name3;
+    @FXML TextField name4;
+    @FXML TextField name5;
+    @FXML TextField add1;
+    @FXML TextField add2;
+    @FXML TextField add3;
+    @FXML TextField add4;
+    @FXML TextField add5;
+    @FXML ProgressBar bar1;
+    @FXML ProgressBar bar2;
+    @FXML ProgressBar bar3;
+    @FXML ProgressBar bar4;
+    @FXML ProgressBar bar5;
+    @FXML Label percentage1;
+    @FXML Label percentage2;
+    @FXML Label percentage3;
+    @FXML Label percentage4;
+    @FXML Label percentage5;
+    @FXML Label hrsWeek;
+    @FXML Label hrsAll;
+    @FXML Label hrsMore;
+    @FXML Label hrsExtra;
+}
